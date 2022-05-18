4f6d61141ca9bf9ecd96f2d48324fd044fa4d759
Kuntal Pal <kpal002@ucr.edu>
2022-05-18 00:08:33 -0700

    Update createPubSchema.sql

--- createPubSchema.sql
+++ createPubSchema.sql
@@ -3,14 +3,14 @@
 -- Create the Publication Schema in SQL
 CREATE TABLE Author (
 		AuthorID INT NOT NULL CONSTRAINT PK_Author PRIMARY KEY,
-		Name varchar(45) NOT NULL,
-		Homepage varchar(200)
+		Name TEXT NOT NULL,
+		Homepage TEXT
 		);
 
 CREATE TABLE Publication (
 		PubID INT NOT NULL CONSTRAINT PK_Publication PRIMARY KEY,
 		PubKey TEXT NOT NULL,
-		Title varchar(45),
+		Title TEXT,
 		Year INT
 		);
 		
@@ -22,31 +22,31 @@ CREATE TABLE Authored (
 		
 CREATE TABLE Article (
 		PubID INT UNIQUE NOT NULL CONSTRAINT FK_Article REFERENCES Publication (PubID),
-		Journal varchar(45),
+		Journal TEXT,
 		Month TEXT,
-		Volume varchar(20),
+		Volume TEXT,
 		Number INT,
 		PRIMARY KEY (PubID)
 		);
 		
 CREATE TABLE Book (
 		PubID INT UNIQUE NOT NULL CONSTRAINT FK_Book REFERENCES Publication (PubID),
-		Publisher varchar(45),
-		ISBN varchar(20),
+		Publisher TEXT,
+		ISBN TEXT,
 		PRIMARY KEY (PubID)
 		);
 		
 CREATE TABLE InCollection (
 		PubID INT UNIQUE NOT NULL CONSTRAINT FK_InCollection REFERENCES Publication (PubID),
-		BookTitle varchar(30),
-		Publisher varchar(20),
-		ISBN varchar(45),
+		BookTitle TEXT,
+		Publisher TEXT,
+		ISBN TEXT,
 		PRIMARY KEY (PubID)
 		);
 		
 CREATE TABLE InProceedings (
 		PubID INT UNIQUE NOT NULL CONSTRAINT FK_InProceedings REFERENCES Publication (PubID),
-		BookTitle varchar(30),
-		Editor varchar(30),
+		BookTitle TEXT,
+		Editor TEXT,
 		PRIMARY KEY (PubID)
 		);
